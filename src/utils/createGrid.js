18f59ceb2535bda7bf540307d85229520cec005a
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-17 14:46:23 -0500

    The grid is already done, but some improvements are needed yet

--- src/utils/createGrid.js
+++ src/utils/createGrid.js
@@ -1,4 +1,4 @@
-let canvas, ctx
+let canvas, ctx, size = 50
 
 const sizeCanvas = (element) => {
     let width = element.offsetWidth;
@@ -22,12 +22,49 @@ const printMainAxis = (width, height) => {
     ctx.restore()
 }
 
+const printVerticalLines = (width, height) => {
+    ctx.strokeStyle = "rgba(0,0,0,0.5)";
+    ctx.lineWidth = 0.5;
+
+    for(let x = size; x < width; x += size){
+        ctx.beginPath()
+        ctx.moveTo(x, -height)
+        ctx.lineTo(x, height)
+        ctx.stroke()
+    }
+
+    for(let x2 = -size; x2 > -width; x2 -= size){
+        ctx.beginPath()
+        ctx.moveTo(x2, -height)
+        ctx.lineTo(x2, height)
+        ctx.stroke()
+    }
+}
+
+const printHorizontalLines = (width, height) => {
+    for(let y = size; y < width; y += size){
+        ctx.beginPath()
+        ctx.moveTo(-width, y)
+        ctx.lineTo(width, y)
+        ctx.stroke()
+    }
+
+    for(let y2 = -size; y2 > -height; y2 -= size){
+        ctx.beginPath()
+        ctx.moveTo(-width, y2)
+        ctx.lineTo(width, y2)
+        ctx.stroke()
+    }
+}
+
 const printGrid = () => {
     let width = canvas.width / 2
     let height = canvas.height / 2
     ctx.translate(width, height)
 
     printMainAxis(width, height)
+    printVerticalLines(width, height)
+    printHorizontalLines(width, height)
 }
 
 const createGrid = (component) => {

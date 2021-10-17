9f497e3bc15e1ac9a138d0656bd06f0c33097cdb
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-17 14:17:47 -0500

    Resizing done successfully

--- src/utils/createGrid.js
+++ src/utils/createGrid.js
@@ -8,16 +8,39 @@ const sizeCanvas = (element) => {
     element.height = height;
 }
 
+const printMainAxis = (width, height) => {
+    ctx.save()
+    ctx.lineWidth = 2;
+
+    ctx.beginPath()
+    ctx.moveTo(-width, 0)
+    ctx.lineTo(width, 0)
+    ctx.moveTo(0, -height)
+    ctx.lineTo(0, height)
+    ctx.stroke()
+
+    ctx.restore()
+}
+
+const printGrid = () => {
+    let width = canvas.width / 2
+    let height = canvas.height / 2
+    ctx.translate(width, height)
+
+    printMainAxis(width, height)
+}
+
 const createGrid = (component) => {
     canvas = document.querySelector("canvas");
     ctx = canvas.getContext("2d")
 
     sizeCanvas(canvas)
-
-    ctx.translate(canvas.width / 2, canvas.height / 2)
-    ctx.beginPath()
-    ctx.rect(0,0,50,50);
-    ctx.stroke()
+    printGrid()
+    
+    canvas.addEventListener("resize", () => {
+        sizeCanvas(canvas)
+        printGrid()
+    })
 }
 
 export default createGrid;
